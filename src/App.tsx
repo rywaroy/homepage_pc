295b4335129e995006789f344099a5e8e9658b61
zhangzhihao <464485682@qq.com>
2018-10-09 17:50:36 +0800

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { Layout } from 'antd';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routers from './router';
 import Menu from './components/Menu/Menu';
+import Loading from './components/Loading/Loading';
+import { Provider } from 'mobx-react';
+import store from './store';
 import 'antd/dist/antd.css';
 import './App.css';
 
@@ -10,33 +14,35 @@ const {Header, Footer, Sider, Content} = Layout;
 class App extends React.Component {
   public render() {
     return (
-      <Router>
-				<Layout style={{minHeight: '100vh'}}>
-					{/* <Loading/> */}
-					<Sider style={{overflow: 'auto', height: '100vh', position: 'fixed', left: 0, zIndex: 5}}>
-						<Menu/>
-					</Sider>
-					<Layout style={{marginLeft: 200}}>
-						<Header>
-							<div className="header">哈哈</div>
-							<div className="header-mobile">
-								手机端
-								<div className="header-mobile-box">
-									<div className="header-mobile-code" />
+			<Provider {...store}>
+				<Router>
+					<Layout style={{minHeight: '100vh'}}>
+						<Loading/>
+						<Sider style={{overflow: 'auto', height: '100vh', position: 'fixed', left: 0, zIndex: 5}}>
+							<Menu/>
+						</Sider>
+						<Layout style={{marginLeft: 200}}>
+							<Header>
+								<div className="header">哈哈</div>
+								<div className="header__mobile">
+									手机端
+									<div className="header__mobile-box">
+										<div className="header__mobile-code" />
+									</div>
 								</div>
-							</div>
-						</Header>
-						<Content>
-              {/* <Routers/> */}
-						</Content>
-						<Footer>
-							<div className="footer">网站大部分 API
-								均由产品公司自身提供，本人皆从网络获取。获取与共享之行为或有侵犯产品权益的嫌疑。若被告知需停止共享与使用，本人会及时删除此页面与整个项目。请您暸解相关情况，并遵守产品协议。
-							</div>
-						</Footer>
+							</Header>
+							<Content>
+								<Routers/>
+							</Content>
+							<Footer>
+								<div className="footer">网站大部分 API
+									均由产品公司自身提供，本人皆从网络获取。获取与共享之行为或有侵犯产品权益的嫌疑。若被告知需停止共享与使用，本人会及时删除此页面与整个项目。请您暸解相关情况，并遵守产品协议。
+								</div>
+							</Footer>
+						</Layout>
 					</Layout>
-				</Layout>
-			</Router>
+				</Router>
+			</Provider>
     );
   }
 }

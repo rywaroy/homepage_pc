9273d95b068e003f5a70275ed277e78056a3287a
zhangzhihao <464485682@qq.com>
2018-10-10 18:23:13 +0800

    Update one

--- src/store/one.ts
+++ src/store/one.ts
@@ -3,11 +3,16 @@ import * as dayjs from 'dayjs';
 
 export default class One {
 
-  @observable public list: any = [];
+  @observable public list: any[] = [];
   @observable public date: string = dayjs().format('YYYY-MM-DD');
 
   @action
-  public setList(array: any[]) {
+  setList(array: any[]) {
     this.list = array;
   }
+
+  @action 
+  setDate(date: string) {
+		this.date = date;
+	}
 }

9e1c3f8b03b0426a7672e0629631cabe00a59516
zhangzhihao <464485682@qq.com>
2018-10-10 16:44:07 +0800

    Fix loading store

--- src/store/loading.ts
+++ src/store/loading.ts
@@ -1,22 +1,22 @@
 import { observable, action } from 'mobx';
 
 export interface ILoading {
-  active:boolean
+  active: boolean
 }
 
 export default class Loading  {
 
-  @observable public active:boolean
+  @observable public active: boolean
 
   constructor() {
     this.active = false;
   }
 
-  @action public show():void {
+  @action public show(): void {
     this.active = true;
   }
 
-  @action public hide():void {
+  @action public hide(): void {
     this.active = false;
   }
 }

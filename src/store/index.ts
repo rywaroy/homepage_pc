6c6569b72d5eb81fb1983d428a160bddc779bd39
zhangzhihao <464485682@qq.com>
2018-10-14 22:22:48 +0800

    Add zhihu

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import Loading from './loading';
 import One from './one';
 import Huaban from './huaban';
+import Zhihu from './zhihu';
 
 const Store = {
   loading: new Loading(),
   one: new One(),
   huaban: new Huaban(),
+  zhihu: new Zhihu(),
 }
 
 export default Store;

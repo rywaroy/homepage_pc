589cbc0b4be5153dbe1ee0e17d950db997c0e2ee
zhangzhihao <464485682@qq.com>
2018-10-31 21:50:57 +0800

    Add album

---
+++ src/router/Album/Album.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Row, Col } from 'antd';
+import { RouteComponentProps } from 'react-router-dom';
+import { observer, inject } from 'mobx-react';
+import axios from '../../utils/axios';
+import albumStore from '../../store/album';
+import { IList } from '../../model/album';
+import './album.css';
+
+interface IPorps extends RouteComponentProps {
+  album: albumStore
+}
+
+@inject('album')
+@observer
+export default class Album extends React.Component<IPorps, any> {
+
+	componentDidMount() {
+		this.props.album.list.length === 0 && this.getList();
+	}
+
+	// 获取相册列表
+	public getList() {
+		axios.get('album/album')
+			.then(res => {
+				this.props.album.setList(res.data.data);
+			});
+	}
+
+	// 跳转到相册详情页
+	public linkInfo(id: number) {
+		this.props.history.push(`/blog/album/group/${id}`);
+	}
+
+	render() {
+		return (
+			<Row>
+				<Col span={2} />
+				<Col span={20}>
+					<div className="album f-cb">
+						{
+							this.props.album.list.map((item: IList, index: number) => (
+								<div className="album__item" key={index} onClick={() => this.linkInfo(item.id)}>
+									<div className="album__item-img bg-cover" style={{ backgroundImage: 'url(' + item.img + ')' }} />
+									<div className="album__item-title">{item.title}</div>
+								</div>
+							))
+						}
+					</div>
+				</Col>
+				<Col span={2} />
+			</Row>
+		);
+	}
+}
